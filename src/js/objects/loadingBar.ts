d0ded3649d8eaca123402c31bd1539a943743528
pixel-fabian <82926714+pixel-fabian@users.noreply.github.com>
2022-04-02 21:18:51 +0200

    :lipstick: UPDATE loading bar

--- src/js/objects/loadingBar.ts
+++ src/js/objects/loadingBar.ts
@@ -2,35 +2,100 @@ export default class LoadingBar {
   private progress = 0;
   private scene: Phaser.Scene;
   private text: Phaser.GameObjects.Text;
-  private background: Phaser.GameObjects.Graphics;
-  private foreground: Phaser.GameObjects.Graphics;
+  private window: Phaser.GameObjects.Graphics;
+  private windowBar: Phaser.GameObjects.Graphics;
+  private windowText: Phaser.GameObjects.Text;
+  private windowX: Phaser.GameObjects.Text;
+  private loadingBarBg: Phaser.GameObjects.Graphics;
+  private loadingBarFg: Phaser.GameObjects.Graphics;
+  private targetWidth = -710;
+  private width = 0;
 
   constructor(scene) {
     this.scene = scene;
-    this.background = this.scene.add.graphics({
-      fillStyle: {
-        color: 0x222222,
-      },
-    });
-    this.background.fillRect(10, 490, 780, 100);
-    this.foreground = this.scene.add.graphics({
-      fillStyle: {
-        color: 0xcccccc,
-      },
-    });
-    const screenCenterX = this.scene.scale.width / 2;
-    this.text = this.scene.add
-      .text(screenCenterX, 530, 'Loading...', {
-        fontFamily: 'BitPotion',
-        color: '#fff',
-        fontSize: '92px',
-      })
-      .setOrigin(0.5);
-    this.text.flipX = true;
+    this._createWindow();
+    this._createLoadingBar();
   }
 
   addProgress(percent: number) {
     this.progress = this.progress + percent;
-    this.foreground.fillRect(785, 495, -770 * this.progress, 90);
+    this.width = this.targetWidth * this.progress;
+    this.loadingBarFg.clear();
+    this.loadingBarFg.fillRect(745, 295, this.width, 90);
+    this.loadingBarFg.setAlpha(0.2);
+  }
+
+  checkFull() {
+    let full = false;
+    if (this.width === this.targetWidth) {
+      full = true;
+    }
+    return full;
+  }
+
+  _createWindow() {
+    this.window = this.scene.add.graphics({
+      fillStyle: {
+        color: 0xdddddd,
+      },
+    });
+    this.window.fillRect(10, 50, 780, 500);
+    this.window.setAlpha(0.2);
+    this.window.lineStyle(3, 0x222222, 1).strokeRect(10, 50, 780, 500);
+
+    this.windowBar = this.scene.add.graphics({
+      fillStyle: {
+        color: 0x000000,
+      },
+    });
+    this.windowBar.fillRect(10, 50, 780, 30);
+    this.windowBar.setAlpha(0.2);
+
+    this.windowText = this.scene.add
+      .text(725, 60, 'Awesome game', {
+        fontFamily: 'BitPotion',
+        color: '#000',
+        fontSize: '24px',
+      })
+      .setOrigin(0.5)
+      .setAlpha(0.2);
+    this.windowText.flipX = true;
+
+    this.windowX = this.scene.add
+      .text(20, 60, 'X', {
+        fontFamily: 'BitPotion',
+        color: '#000',
+        fontSize: '24px',
+      })
+      .setOrigin(0.5)
+      .setAlpha(0.2);
+  }
+
+  _createLoadingBar() {
+    this.loadingBarBg = this.scene.add.graphics({
+      fillStyle: {
+        color: 0xbbbbbb,
+      },
+    });
+    this.loadingBarBg.fillRect(30, 290, 720, 100);
+    this.loadingBarBg.setAlpha(0.2);
+
+    this.loadingBarFg = this.scene.add.graphics({
+      fillStyle: {
+        color: 0xee5533,
+      },
+    });
+    this.loadingBarFg.setAlpha(0.2);
+
+    const screenCenterX = this.scene.scale.width / 2;
+    this.text = this.scene.add
+      .text(screenCenterX, 330, 'Loading...', {
+        fontFamily: 'BitPotion',
+        color: '#000',
+        fontSize: '92px',
+      })
+      .setOrigin(0.5)
+      .setAlpha(0.2);
+    this.text.flipX = true;
   }
 }
